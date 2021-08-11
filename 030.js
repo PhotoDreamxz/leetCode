1b75700ab2f6ea6b24f78d4dc7346fb5f076b130
xiaozheng <3103407342@qq.com>
2021-08-11 23:41:28 +0800

    leetCode

---
+++ 030.js
@@ -0,0 +1,36 @@
+/**
+ * 搜索旋转排序数组
+ * @param {*} nums 
+ * @param {*} target 
+ */
+//简单遍历，不推荐
+//  var search = function(nums, target) {
+//     var key = -1
+//     for(var i = 0;i < nums.length;i++){
+//         if(nums[i] === target){
+//             key = i
+//             break
+//         }
+//     }
+//     return key
+// };
+var search = function(nums, target) {
+    if(nums.length === 1){
+        if(nums[0] === target){
+            return 0
+        }else{
+            return -1
+        }
+    }else{
+       var low = 0
+       var heigh = nums.length - 1
+
+
+    }
+    
+};
+
+function binarySearch(low,heigh){
+
+}
+
